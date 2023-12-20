6a1a2b36b6fb05fb6e40e8a585a6a12a53a0042f
HanJhj <148518753+HanJhj@users.noreply.github.com>
2023-12-20 23:36:58 +0800

    hj-front-核污染现状-日本排放时间线-model

---
+++ yii-advanced-app-2.0.32/advanced/frontend/models/TimelineQuery.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Team: 不想重装队
+ * Coding By: 姜涵
+ * 本文件用于新闻界面
+ */
+namespace frontend\models;
+
+/**
+ * This is the ActiveQuery class for [[Timeline]].
+ *
+ * @see Timeline
+ */
+class TimelineQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return Timeline[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return Timeline|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
