c1bea3104ba0623c5cdbd43dd4f13e779dffabe0
aurora-er <143080974+aurora-er@users.noreply.github.com>
2023-12-20 22:22:47 +0800

    syh-前台-影响-国际关系页面

---
+++ yii-advanced-app-2.0.32/advanced/frontend/views/internationalrelation/create.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Team: NKU-不想重装
+ * Coding By: 师雅卉
+ * 本文件用于Internationalrelation界面
+ */
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Internationalrelation */
+
+$this->title = 'Create Internationalrelation';
+$this->params['breadcrumbs'][] = ['label' => 'Internationalrelations', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="internationalrelation-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ yii-advanced-app-2.0.32/advanced/frontend/views/internationalrelation/index.php
@@ -0,0 +1,247 @@
+<?php
+/**
+ * Team: NKU-不想重装
+ * Coding By: 师雅卉
+ * 本文件用于Internationalrelation界面
+ */
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\InternationalrelationSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Internationalrelations';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<head>
+
+<!-- Meta -->
+<meta charset="utf-8">
+
+
+<!-- The compiled CSS file -->
+<link rel="stylesheet" href="static3/css/production.css">
+
+</head>
+
+
+
+<body class="has-animations" style="height: 100%;">
+
+
+<!-- Header -->
+<header class="pt2 pb1 align--center">
+<div class="container">
+    <h1 title="Camp Chippewa" style="font-family: 华文新魏;color:DarkSlateGray">核污染对国际关系的影响</h1>
+    <p>The Impact of the Nuclear contamination on International Relation</p>
+</div>
+</header>
+
+
+
+
+
+<!-- Body -->
+<main>
+
+<div class="container">
+
+    <!-- Info -->
+    <section class="grid-row grid-row--center">
+        <!-- Cabin -->
+        <div class="grid-column span-half pt3 pb3 mobile-m order-1 reveal-on-scroll is-revealing" style="visibility: visible;">
+            <div class="relative">
+                <img class="info-image relative z2" src="static3/img/fm.jpg" alt="Cabin">
+                <div class="pattern pattern--left-down absolute z1"></div>
+            </div>
+        </div>
+        <div class="grid-column span-half pt3 pb3 mobile-m order-2">
+            <p>俄乌战争对国际关系格局产生了巨大影响。尤其是，在俄罗斯与西方国家之间存在着巨大分歧的情况下，俄欧关系却一直保持相对稳定，并没有出现严重冲突或危机。这是因为两国拥有良好的政治互信基础和经济互补性。俄罗斯和欧洲之间的战争已经持续了几个世纪，直到冷战结束才有所缓和，近年来，随着能源合作的深化，双方的关系进一步缓和，但俄乌战争和乌克兰战争的爆发中断了这一过程，两国关系可能重新滑入冷战时期。</p>
+        </div>
+        <!-- Teepees -->
+
+
+
+        <?php foreach ($Internationalrelation as $ir) : ?>
+        <div class="grid-column span-half pt3 pb3 mobile-m order-4">
+            <h3><?= $ir->name ?></h3>
+            <p><?= $ir->influence ?></p>
+        </div>
+        <div class="grid-column span-half pt3 pb3 mobile-m order-3 reveal-on-scroll is-revealing" style="visibility: visible;">
+            <div class="relative">
+                <img class="info-image relative z2" src="static3/img/<?=$ir->picture?>" alt="Teepee">
+                <div class="pattern pattern--right-middle absolute z1"></div>
+            </div>
+
+            <div class="relative">
+                <img class="info-image relative z2" src="static3/img/<?=$ir->picture2?>" alt="Teepee">
+                <div class="pattern pattern--right-middle absolute z1"></div>
+            </div>
+
+            <div class="relative">
+                <img class="info-image relative z2" src="static3/img/<?=$ir->picture3?>" alt="Teepee">
+                <div class="pattern pattern--right-middle absolute z1"></div>
+            </div>
+
+        </div>
+
+        <?php endforeach; ?>
+
+
+
+
+
+    </section>
+
+</div>
+
+<!-- Pricing -->
+<section class="align--center pt2 pb3">
+    <p class="h2">Nuclear contamination</p>
+    <p>International Relation</p>
+</section>
+
+
+<!--footer-->
+		   <footer>
+		<div class="instagram-posts">
+			<h3 class="tittle cen foot">保护地球，就是保护我们自己的未来，让绿意蔓延，让生态永续。</h3>
+			<h3 class="tittle cen foot">Protecting the Earth is about safeguarding our own future, fostering greenery, and ensuring ecological sustainability.</h3>
+			<div class="row footer-top">
+				<!-- instagram posts -->
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://zh.wikipedia.org/w/index.php?search=%E6%97%A5%E6%9C%AC%E6%A0%B8%E6%B1%A1%E6%B0%B4&title=Special%3A%E6%90%9C%E7%B4%A2&ns0=1">
+						<img src="statics/images/i1.jpeg" class="img-fluid" alt="" />
+					</a>
+				</div>
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://kns.cnki.net/kns8s/defaultresult/index?crossids=YSTT4HG0%2CLSTPFY1C%2CJUP3MUPD%2CMPMFIG1A%2CWQ0UVIAA%2CBLZOG7CK%2CEMRPGLPA%2CPWFIRAGL%2CNLBO1Z6R%2CNN3FJMUV&korder=SU&kw=%E6%A0%B8%E6%B1%A1%E6%9F%93">
+					<img src="statics/images/i2.jpeg" class="img-fluid" alt="" />
+					</a>
+				</div>
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://www.zhihu.com/search?type=content&q=%E6%A0%B8%E6%B1%A1%E6%9F%93">
+					<img src="statics/images/f2.png" class="img-fluid" alt="" />
+					</a>
+				</div>
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://www.youtube.com/results?search_query=%E6%A0%B8%E6%B1%A1%E6%9F%93">
+					<img src="statics/images/f4.png" class="img-fluid" alt="" />
+					</a>
+				</div>
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://www.bbc.co.uk/search?q=nuclear+contamination&seqId=91234710-9f37-11ee-8eb0-51c49da9aced&d=HOMEPAGE_GNL">
+					<img src="statics/images/f5.png" class="img-fluid" alt="" />
+					</a>
+				</div>
+				<div class="col-md-2 col-sm-2 footer-grid-img">
+					<a href="https://so.news.cn/#search/0/%E6%A0%B8%E6%B1%A1%E6%9F%93/1/">
+					<img src="statics/images/i6.jpeg" class="img-fluid" alt="" />
+					</a>
+				</div>
+			</div>
+
+			<div class="col-md-3 agileinfo_footer_grid">
+			<h3><p style="color:white">Introduction</p></h3>
+				<p >本网站旨在呼吁保护生态环境，通过向大众科普核污染知识，提供地球的核污染现状，回溯历史进行案例分析，从各方面展现核污染的危害，用行动捍卫地球。</span></p>
+				<div class="w3ls-social-icons-2">
+				<a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+				<a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+				<a class="pinterest" href="#"><i class="fa fa-google-plus"></i></a>
+				<a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+				<a class="tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+				</div>
+			</div>
+
+
+			<div class="col-md-3 agileinfo_footer_grid f3">
+				<h3 ><p style="color:white">导航</p></h3>
+				<div class="nav-w3-l">
+				<ul>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="../../backend/web" >登录后台</a></li>
+					<li><a href="index.php?r=site/about" >关于团队</a></li>
+					<li><a href="index.php?r=casestudy" >案例研究</a></li>
+					<li><a href="index.php?r=news" >核污染新闻</a></li>
+					<li><a href="index.php?r=timeline" >日本核污水排放时间线</a></li>
+					<li><a href="index.php?r=energy" >影响-生态环境</a></li>
+					<li><a href="index.php?r=economics" >影响-社会经济</a></li>
+					<li><a href="index.php?r=internationalrelation" >影响-国际关系</a></li>
+					<li><a href="index.php?r=comment" >各界观点</a></li>
+				</ul>
+				</div>
+			</div>
+
+
+
+			<div class="col-md-3 agileinfo_footer_grid f3">
+				<h3 ><p style="color:white">相关网站</p></h3>
+				<div class="nav-w3-l">
+				<ul>
+					<li><a href="https://www.wikipedia.org/" >维基百科</a></li>
+					<li><a href="https://www.nrc.gov/" >美国核能管理委员会NRC</a></li>
+					<li><a href="https://www.iaea.org/" >国际原子能机构IAEA</a></li>
+					<li><a href="https://www.youtube.com" >youtube</a></li>
+					<li><a href="https://www.bbc.com/" >BBC</a></li>
+					<li><a href="https://www.bilibili.com/" >bilibili</a></li>
+					<li><a href="https://www.zhihu.com/" >知乎</a></li>
+					<li><a href="https://ec.europa.eu/euratom/index_en.htm" >欧洲原子能共同体EURATOM</a></li>
+				</ul>
+				</div>
+			</div>
+
+			<div class="col-md-3 agileinfo_footer_grid">
+			<h3><p style="color:white">We are</p></h3>
+				<p>互联网数据库开发课程-不想重装队，已经历经所有报错，立志coding时不重装环境！</p>
+				<div class="w3ls-social-icons-2">
+				<a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+				<a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+				<a class="pinterest" href="#"><i class="fa fa-google-plus"></i></a>
+				<a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+				<a class="tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+				</div>
+			</div>
+
+			<div class="footer-bottom-w3layouts-agileinfo">
+				<!-- //subscribe -->
+				<!-- footer -->
+				<div class="footer-cpy text-center">
+					<div class="footer-social">
+						<div class="copyrighttop">
+							<ul>
+								<li>
+									<a class="facebook" href="#">
+										<i class="fab fa-facebook-f"></i>
+									</a>
+								</li>
+								<li>
+									<a class="facebook" href="#">
+										<i class="fab fa-twitter"></i>
+									</a>
+								</li>
+								<li>
+									<a class="facebook" href="#">
+										<i class="fab fa-google-plus-g"></i>
+									</a>
+								</li>
+								<li>
+									<a class="facebook" href="#">
+										<i class="fab fa-pinterest-p"></i>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+					</div>
+					<div class="copyrightbottom-w3ls-agile">
+						<p>Copyright &copy; 2023. NKU-不想重装队 All rights reserved.<a target="_blank" href="https://github.com/ValerieLuo2003/InternetDatabaseDevelopment">源代码Github链接</a></p>
+
+					</div>
+				</div>
+				<!-- //footer -->
+			</div>
+		</div>
+</footer>
+
+	<!---->
