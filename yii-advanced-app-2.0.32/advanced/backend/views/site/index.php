a4aa52cca1553ffb1d6cd3f67cae9e4a24a19a50
Luo Ziyang <139753604+ValerieLuo2003@users.noreply.github.com>
2023-12-21 20:39:13 +0800

    罗梓洋、

---
+++ yii-advanced-app-2.0.32/advanced/backend/views/site/index.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Team: 不想重装队
+ * Coding By: 罗梓洋
+ * 本文件用于后台主页
+ */
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1><p align="center">欢迎登录后台!</p></h1>
+
+        <p align="center"style="font-size:20px">Welcome to the backend</p>
+    </div>
+<div class="contain">
+    <body>
+        <p style="text-align: center;">
+            <img src="static/picture/nku.png" alt="">
+        </p>
+    </body>
+    <h2><p align="center"style="color:purple">开发团队介绍</p></h2>
+</div>
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-6">
+                <h3><p style="color:navy">罗梓洋</p></h3>
+                
+                <p>  </p>
+                <p>完成前台案例研究、各界观点页面，以及相应的后台数据管理</p>
+                <p>完成后台模板的导入、主页、个人作业界面</p>
+                <p>完成了对应于数据表casestudy、comment、personhomework的MVC结构</p>
+                <p>负责文档：实现文档、项目展示PPT</p>
+
+                <p><a class="btn btn-default" href="https://github.com/ValerieLuo2003">进入Github &raquo;</a></p>
+            </div>
+            <div class="col-lg-6">
+            <h3><p style="color:navy">李晓彤</p></h3>
+
+                <p>完成前台核污染对生态环境、社会经济影响的页面，以及相应的后台数据管理</p>
+                <p>完成后台团队作业界面</p>
+                <p>完成了对应于数据表ecological、economics的MVC结构</p>
+                <p>负责文档：实现文档、部署文档</p>
+
+                <p><a class="btn btn-default" href="https://github.com/enertiy">进入Github &raquo;</a></p>
+            </div>
+            <div class="col-lg-6">
+            <h3><p style="color:navy">姜涵</p></h3>
+                <p>  </p>
+                <p>完成前台核污染现状的新闻、时间线页面，以及相应的后台数据管理</p>
+                <p>完成对应于数据表news的MVC结构，并完成了爬虫，对news数据表的数据爬取</p>
+                <p>完成后台用户管理页面</p>
+                <p>实现一键部署功能</p>
+                <p>负责文档：实现文档、用户手册</p>
+                <p><a class="btn btn-default" href="https://github.com/HanJhj">进入Github &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-6">
+            <h3><p style="color:navy">师雅卉</p></h3>
+                <p>  </p>
+                <p>完成前台模板的导入，前台导航栏和主页</p>
+                <p>完成前台核污染对国际关系影响的页面，以及相应的后台数据管理</p>
+                <p>完成了对应于数据表internationalrelation的MVC结构</p>
+                <p>负责文档：实现文档、需求文档、设计文档</p>
+
+                <p><a class="btn btn-default" href="https://github.com/aurora-er">进入Github &raquo;</a></p>
+            </div>
+        </div>
+        <p align="center"><a class="btn btn-lg btn-info" href="http://www.yiiframework.com">进入 Yii 官网</a></p>
+
+    </div>
+</div>
