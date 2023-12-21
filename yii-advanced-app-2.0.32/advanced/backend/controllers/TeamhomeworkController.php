daffa14c6b05a36163a0977b1b50d35351fe6a8b
enertiy <113110661+enertiy@users.noreply.github.com>
2023-12-21 19:50:11 +0800

    lxt-后台-团队作业下载

---
+++ yii-advanced-app-2.0.32/advanced/backend/controllers/TeamhomeworkController.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Team: NKU-不想重装
+ * Coding By: 李晓彤
+ * 本代码用于后台团队作业下载
+ */
+namespace backend\controllers;
+
+use Yii;
+use backend\models\Teamhomework;
+use backend\models\TeamhomeworkSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\data\Pagination;
+use yii\db\ActiveRecord;
+use yii\data\Sort;
+$Teamhomework = Teamhomework::find() -> orderBy('num_id') -> all();
+
+/**
+ * TeamhomeworkController implements the CRUD actions for Teamhomework model.
+ */
+class TeamhomeworkController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $layout = "main_layout";
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Teamhomework models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new TeamhomeworkSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        $query = Teamhomework::find();
+        $pagination = new Pagination([
+            'totalCount' =>  $query-> count(),
+            'pageSize' =>  10            
+          ]);
+        $Teamhomework = $query-> orderBy('num_id')
+        -> offset($pagination-> offset)
+        -> limit($pagination-> limit)
+        -> all();
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'Teamhomework' => $Teamhomework,
+            'pagination' => $pagination
+        ]);
+    }
+
+    /**
+     * Displays a single Teamhomework model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Teamhomework model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Teamhomework();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->num_id]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Teamhomework model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->num_id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Teamhomework model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Teamhomework model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Teamhomework the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Teamhomework::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
