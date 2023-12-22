f8808ad0ef2e3d1ade99224c8e97477bec1e1694
HanJhj <148518753+HanJhj@users.noreply.github.com>
2023-12-22 19:25:03 +0800

    小修改

---
+++ yii-advanced-app-2.0.32/advanced/frontend/config/main-local.php
@@ -0,0 +1,13 @@
+<?php 
+ $config = [ 
+
+    'components' => [  
+
+        'request' => [  
+
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation 
+
+            'cookieValidationKey' => 'lXfAJ_cFiUH-Ze0SsHpXrpmwmn_sAonW', 
+ ], 
+], 
+];   return $config;
