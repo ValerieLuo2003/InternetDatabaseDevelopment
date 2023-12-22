6b583470854babaf6401ce4febf185feeace5b35
HanJhj <148518753+HanJhj@users.noreply.github.com>
2023-12-22 19:22:26 +0800

    小修改

--- yii-advanced-app-2.0.32/advanced/frontend/assets/AppAsset.php
+++ yii-advanced-app-2.0.32/advanced/frontend/assets/AppAsset.php
@@ -10,14 +10,35 @@ use yii\web\AssetBundle;
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $baseUrl = '@web/statics'; 
     public $css = [
-        'css/site.css',
+        //'css/site.css',
+        'css/bootstrap.css',
+        'css/style.css',
+        'newscss/style.css',
+        'newscss/font-awesome.min.css',
+        'newscss/bootstrap.min.css',
+        'css/prettyPhoto.css',
+        'css/owl.carousel.css',
+        'css/owl.theme.css',
+        'css/fontawesome-all.css'
     ];
     public $js = [
+        'js/jquery-2.2.3.min.js',
+        'js/bootstrap.js',
+        'js/owl.carousel.js',
+        'js/move-top.js',
+        'js/easing.js',
+        'js/jquery-1.7.2.js',
+        'js/jquery.quicksand.js',
+        'js/script.js',
+        'js/jquery.prettyPhoto.js',
+        'js/newsjs/bootstrap.min.js',
+        'js/newsjs/jquery.min.js',
+        'js/newsjs/main.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\web\YiiAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 }
